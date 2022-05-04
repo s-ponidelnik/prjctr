91d0f6832be511e56e2a1353fd34c2b76015b408
Serhii Ponidielnik <serhii.ponidielnik@pmi.bet>
2022-05-04 18:50:58 +0300

    prjctr

---
+++ README.md
@@ -0,0 +1,9 @@
+# basic-tig-nginx-php-mongo-elasticsearch
+
+### Basic example project to show the TIG (Telegraf, InfluxDB and Grafana) stack with php-fpm, mongo, nginx, elasticsearch
+Screenshots of the project:
+![image info](./mongo.png)
+![image info](./network.png)
+![image info](./elasticsearch.png)
+![image info](./php-fpm.png)
+![image info](./nginx.png)
---
+++ var/www/src/Controller/GampController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use FourLabs\GampBundle\FourLabsGampBundle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use TheIconic\Tracking\GoogleAnalytics\Analytics;
+
+class GampController extends AbstractController
+{
+    public function __construct()
+    {
+
+    }
+    public function index(): Response
+    {
+
+
+        // Instantiate the Analytics object
+        // optionally pass TRUE in the constructor if you want to connect using HTTPS
+        $analytics = new Analytics(false);
+    //https://www.google-analytics.com/mp/collect?measurement_id=G-HY70DVNZXM&api_secret=EVgnlEc8Sc65By6IyTy80g
+        //{"client_id":"3540034146","events":[{"name":"tutorial_begin","params":{"currency":"USD","ratio":30.39}}]}
+        // Build the GA hit using the Analytics class methods
+        // they should Autocomplete if you use a PHP IDE
+        $analytics
+            ->setProtocolVersion('1')
+            ->setTrackingId('G-HY70DVNZXM')
+            ->setClientId('3540034146')
+            ->setUserAgentOverride('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36')
+
+            ->setIpOverride("202.126.106.175");
+
+        // When you finish bulding the payload send a hit (such as an pageview or event)
+        $res=$analytics->sendPageview();
+
+        $response = new Response();
+        $response->setContent(var_export($res, true));
+        $response->setStatusCode(200);
+
+        return $response;
+    }
+
+}
